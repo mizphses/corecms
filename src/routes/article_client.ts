4f55bc76b42c404125c1f901d6fe0b2188502ef4
mizphses <fuminori@mizphs.es>
2024-05-28 19:22:01 +0900

    type fix

--- src/routes/article_client.ts
+++ src/routes/article_client.ts
@@ -5,9 +5,9 @@ import type { Env } from "../env"
 
 const article = new Hono<{ Bindings: Env }>()
 
-article.get("/", (c) => {
+article.get("/", async (c) => {
   const prisma = new PrismaClient({ adapter: new PrismaD1(c.env.CORECMS_DB) })
-  const articles = prisma.post.findMany({
+  const articles = await prisma.post.findMany({
     select: {
       id: true,
       title: true,
@@ -22,54 +22,10 @@ article.get("/:id", (c) => {
   const id = c.req.param("id")
   const article = prisma.post.findUnique({
     where: {
-      id: Number.parseInt(id),
+      id: id,
     },
   })
-  const tags = prisma.tag.findMany({
-    where: {
-      postId: Number.parseInt(id),
-    },
-  })
-  const categories = prisma.category.findMany({
-    where: {
-      postId: Number.parseInt(id),
-    },
-  })
-  return c.json({
-    id: article.id,
-    title: article.title,
-    content: article.content,
-    author: {
-      id: article.author.id,
-      name: article.author.name,
-    },
-    tags: tags.map(
-      (tag: {
-        id: string
-        name: string
-      }) => ({
-        id: tag.id,
-        name: tag.name,
-      }),
-    ),
-    categories: categories.map(
-      (category: {
-        id: string
-        name: string
-      }) => ({
-        id: category.id,
-        name: category.name,
-      }),
-    ),
-    issueAt: article.issueAt,
-    updated_at: article.updatedAt,
-    openGraph: {
-      title: article.openGraph.title,
-      description: article.openGraph.description,
-      image: article.openGraph.image,
-      twitterType: article.openGraph.twitterType,
-    },
-  })
+  return c.json(article)
 })
 
 export default article
